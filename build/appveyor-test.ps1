fb3e351a21f7e5215ff5b120fcd11c6f18e4916f
Ilya Finkelshteyn <ilyaf@appveyor.com>
2018-05-09 11:20:23 -0700

    back to mstest

--- build/appveyor-test.ps1
+++ build/appveyor-test.ps1
@@ -3,5 +3,5 @@ dotnet test -c $env:CONFIGURATION --logger:trx --no-build .\tests\Swashbuckle.Ex
 $wc = New-Object 'System.Net.WebClient'
 $results = Resolve-Path .\tests\Swashbuckle.Examples.Auto.Tests\TestResults\*.trx
 $result = $results[0]
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)", ($result))
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$($env:APPVEYOR_JOB_ID)", ($result))
 Write-Host 'Uploaded test result file' $result
